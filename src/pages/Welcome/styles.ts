b4c4cc75b71f7a0f1d4c49a072124bff44ad5661
Marlon Tereziano <80834228+MarlonTereziano@users.noreply.github.com>
2022-08-31 14:47:06 -0300

    feat: Initial Pages design with Antd

--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: #blue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: white;
 `
 
 
