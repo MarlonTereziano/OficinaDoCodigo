efeecf819a437f55899095233eb720aba49bd6ae
Marlon Tereziano <80834228+MarlonTereziano@users.noreply.github.com>
2022-11-12 21:26:35 -0300

    feat: Blockly added Sucessfull!

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,42 @@
+import React, { useState, useLayoutEffect } from "react";
+
+import { BlocklyWorkspace } from "react-blockly";
+import Blockly from "blockly";
+import ConfigFiles from "./initContent/content";
 import { Container } from "./styles";
-export const HomePage = () => {
+
+export function HomePage() {
+  const [javascriptCode, setJavascriptCode] = useState("");
+
+  let alertVar = 0;
+  function workspaceDidChange(workspace: any) {
+    try {
+      const code = Blockly.JavaScript.workspaceToCode(workspace);
+      setJavascriptCode(code);
+    } catch (e) {
+      if (alertVar === 0) {
+        alertVar += 1;
+        alert("Esses blocos não encaixam!");
+        console.log("Esses blocos não encaixam!");
+      }
+    }
+  }
 
   return (
-      <Container>
-        aaa
-      </Container>
+    <Container>
+      <BlocklyWorkspace
+        toolboxConfiguration={ConfigFiles.INITIAL_TOOLBOX_JSON}
+        className="fill-height"
+        workspaceConfiguration={{
+          grid: {
+            spacing: 20,
+            length: 3,
+            colour: "#ccc",
+            snap: true,
+          },
+        }}
+        onWorkspaceChange={workspaceDidChange}
+      />
+    </Container>
   );
-};
+}
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -3,5 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: red;
+
+  .fill-height {
+    height: 80vh;
+    width: 60vw;
+  }
+  #code {
+    height: 80vh;
+    width: 30vw;
+    resize: vertical;
+  }
 `;
