c4c6908bbbdbca961944a3f5e83320a4c678fd53
Marlon Tereziano <80834228+MarlonTereziano@users.noreply.github.com>
2023-01-11 18:40:15 -0300

    fix: backgrounds test

--- src/pages/Exercises/Exercise2/styles.ts
+++ src/pages/Exercises/Exercise2/styles.ts
@@ -30,9 +30,9 @@ export const Container = styled.div`
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   }
 
-  .blocklyToolboxContents{
+  /* .blocklyToolboxContents{
     background-color: red;
-  }
+  } */
 
   .blocklyToolboxDiv {
     background-color: white;
