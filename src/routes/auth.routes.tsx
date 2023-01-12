36ea5942b47b3b0aef856842127a5224bbe90f4b
Marlon Tereziano <80834228+MarlonTereziano@users.noreply.github.com>
2023-01-12 16:27:50 -0300

    fix: delete unusable pages

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HomePage } from "../pages/HomePage";
 import { Welcome } from "../pages/Welcome";
 import { Exercise1 } from "../pages/Exercises/Exercise1";
 import { Exercise2 } from "../pages/Exercises/Exercise2";
@@ -8,10 +7,9 @@ export default function AuthRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<HomePage/>} />
-        <Route path="/" element={<Welcome/>} />
-        <Route path="/ex1" element={<Exercise1/>} />
-        <Route path="/ex2" element={<Exercise2/>} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/ex1" element={<Exercise1 />} />
+        <Route path="/ex2" element={<Exercise2 />} />
       </Routes>
     </BrowserRouter>
   );
